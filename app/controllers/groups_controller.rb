4bc0a95f78e353f4cc495bbb8dba2d1d0e2c98d0
你的名字 <yix2730555@yahoo.com>
2017-04-01 21:10:50 +0800

    check owner permission when access edit/update/destroy

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,5 @@
 class GroupsController < ApplicationController
-  before_action :authenticate_user! , only: [:new, :create]
+    before_action :authenticate_user! , only: [:new, :create, :edit, :update, :destroy]
   def index
     @groups = Group.all
   end
@@ -10,7 +10,12 @@ class GroupsController < ApplicationController
 
   def edit
     @group = Group.find(params[:id])
+
+    if current_user != @group.user
+      redirect_to root_path, alert: "You have no permission."
+    end
   end
+
 
   def new
     @group = Group.new
@@ -30,16 +35,26 @@ class GroupsController < ApplicationController
   def update
     @group = Group.find(params[:id])
 
-    @group.update(group_params)
+    if current_user != @group.user
+      redirect_to root_path, alert: "You have no permission"
+    end
 
-    redirect_to groups_path, notice: "Update Success"
+    if @group.update(group_params)
+      redirect_to groups_path, notice: "Update success"
+    else
+      render :edit
+    end
   end
 
     def destroy
       @group = Group.find(params[:id])
+
+      if current_user != @group.user
+        redirect_to root_path, alert: "You have no permission"
+      end
+
       @group.destroy
-      flash[:alert] = "Group deleted"
-      redirect_to groups_path
+      redirect_to groups_path, alert: "Group deleted"
     end
 
   private
@@ -47,5 +62,4 @@ class GroupsController < ApplicationController
   def group_params
     params.require(:group).permit(:title, :description)
   end
-
 end
